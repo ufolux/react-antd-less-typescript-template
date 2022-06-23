3a32895f65eedf8aa06e1a58515a787ebb853f49
ufolux <richadlu001@gmail.com>
2022-06-23 02:39:22 -0400

    feat: config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
-import IndexRouter from '@/components/indexRouter'
-import './App.less'
+import React from 'react';
+import logo from './logo.svg';
+import styles from './App.less';
+import {Button} from 'antd'
 
-function App () {
-  return <IndexRouter />
+function App() {
+  return (
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <img src={logo} className={styles.AppLogo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <Button type='primary' shape='round'>
+          Test
+        </Button>
+        <a
+          className={styles.AppLink}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export const pluralize = (num: number, singular: string, plural: string) => {
-  return num === 1 ? singular : plural
-}
